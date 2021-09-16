28a9134b8995a4dcbeb08c16e6e77895bf69e983
Shimi9999 <moguranian999@gmail.com>
2021-09-16 22:48:31 +0900

    Refactoring: Add BmsCheckerWindow struct, and separate processing

--- main.go
+++ main.go
@@ -12,124 +12,165 @@ import (
 	"github.com/therecipe/qt/widgets"
 )
 
+type BmsCheckerWindow struct {
+	widgets.QMainWindow
+
+	language string
+
+	base *widgets.QWidget
+
+	setting      *widgets.QMenu
+	languageMenu *widgets.QMenu
+	actionEn     *widgets.QAction
+	actionJa     *widgets.QAction
+
+	menu        *widgets.QWidget
+	openButton  *widgets.QPushButton
+	pathInput   *widgets.QLineEdit
+	diffCheck   *widgets.QCheckBox
+	checkButton *widgets.QPushButton
+	fileDialog  *widgets.QFileDialog
+
+	logText    *widgets.QTextEdit
+	setLogText bool
+
+	progressSnake *widgets.QLabel
+}
+
+var app *widgets.QApplication
+
 func main() {
 	varsion := "1.3.0"
-	language := "en"
+	defaultLanguage := "en" // TODO Jsonの設定ファイルを生成し、そこから言語設定を取得する
 
 	core.QCoreApplication_SetApplicationName("BMSChecker")
 	core.QCoreApplication_SetOrganizationName("Shimi9999")
 	core.QCoreApplication_SetAttribute(core.Qt__AA_EnableHighDpiScaling, true)
 
-	app := widgets.NewQApplication(len(os.Args), os.Args)
+	app = widgets.NewQApplication(len(os.Args), os.Args)
 
-	window := widgets.NewQMainWindow(nil, 0)
+	window := NewBmsCheckerWindow(nil, 0)
 	window.SetWindowTitle(fmt.Sprintf("BMSChecker %s", varsion))
 	window.Resize2(700, 450)
 	window.SetMinimumSize2(300, 250)
 	window.SetAcceptDrops(true)
 
-	base := widgets.NewQWidget(nil, 0)
-	base.SetLayout(widgets.NewQVBoxLayout())
-	window.SetCentralWidget(base)
+	window.menuBar()
 
-	setting := window.MenuBar().AddMenu2("Setting")
-	languageMenu := setting.AddMenu2("Language")
+	window.base = widgets.NewQWidget(nil, 0)
+	window.base.SetLayout(widgets.NewQVBoxLayout())
+	window.SetCentralWidget(window.base)
+
+	window.mainMenu()
+
+	window.logTextArea()
+
+	window.progressSnake = widgets.NewQLabel(nil, 0)
+	window.progressSnake.SetMovie(gui.NewQMovie3(":/qml/images/snake.gif", core.NewQByteArray(), nil))
+	window.progressSnake.Movie().Start()
+	window.progressSnake.Hide()
+	window.base.Layout().AddWidget(window.progressSnake)
+
+	window.setLanguage(defaultLanguage)
+
+	window.ConnectDragEnterEvent(func(e *gui.QDragEnterEvent) {
+		if e.MimeData().HasUrls() {
+			e.AcceptProposedAction()
+		}
+	})
+	window.ConnectDropEvent(func(e *gui.QDropEvent) {
+		if e.MimeData().HasUrls() {
+			switch len(e.MimeData().Urls()) {
+			case 1:
+				window.pathInput.SetText(e.MimeData().Urls()[0].ToLocalFile())
+				window.execCheck()
+			case 2:
+				path1, path2 := e.MimeData().Urls()[0].ToLocalFile(), e.MimeData().Urls()[1].ToLocalFile()
+				window.execDiffBmsDir(path1, path2)
+			}
+		}
+	})
+
+	window.Show()
+
+	widgets.QApplication_Exec()
+}
+
+func (w *BmsCheckerWindow) menuBar() {
+	w.setting = w.MenuBar().AddMenu2("Setting")
+	w.languageMenu = w.setting.AddMenu2("Language")
 	langActGroup := widgets.NewQActionGroup(nil)
 	langActGroup.SetExclusive(true)
-	enAct := langActGroup.AddAction2("English")
-	enAct.SetCheckable(true)
-	if language == "en" {
-		enAct.SetChecked(true)
-	}
-	jaAct := langActGroup.AddAction2("日本語")
-	jaAct.SetCheckable(true)
-	if language == "ja" {
-		enAct.SetChecked(true)
-	}
-	var changeLanguage func(lang string)
+	w.actionEn = langActGroup.AddAction2("English")
+	w.actionEn.SetCheckable(true)
+	w.actionJa = langActGroup.AddAction2("日本語")
+	w.actionJa.SetCheckable(true)
 	langActGroup.ConnectTriggered(func(action *widgets.QAction) {
 		switch action.Pointer() {
-		case enAct.Pointer():
-			language = "en"
-		case jaAct.Pointer():
-			language = "ja"
+		case w.actionEn.Pointer():
+			w.setLanguage("en")
+		case w.actionJa.Pointer():
+			w.setLanguage("ja")
 		}
-		changeLanguage(language)
 	})
-	languageMenu.AddActions(langActGroup.Actions())
+	w.languageMenu.AddActions(langActGroup.Actions())
+}
 
-	menu := widgets.NewQWidget(nil, 0)
-	menu.SetLayout(widgets.NewQHBoxLayout())
-	menu.Layout().SetContentsMargins(0, 0, 0, 0)
-	base.Layout().AddWidget(menu)
-
-	logText := widgets.NewQTextEdit(nil)
-	logText.SetFontPointSize(11.0)
-	logText.SetReadOnly(true)
-	logText.SetLineWrapMode(widgets.QTextEdit__NoWrap)
-	logText.SetHorizontalScrollBar(widgets.NewQScrollBar2(core.Qt__Horizontal, nil))
-	logText.SetPlaceholderText("Drag and drop bms file/folder!")
-	base.Layout().AddWidget(logText)
-
-	progressSnake := widgets.NewQLabel(nil, 0)
-	progressSnake.SetMovie(gui.NewQMovie3(":/qml/images/snake.gif", core.NewQByteArray(), nil))
-	progressSnake.Movie().Start()
-	progressSnake.Hide()
-	base.Layout().AddWidget(progressSnake)
+func (w *BmsCheckerWindow) mainMenu() {
+	w.menu = widgets.NewQWidget(nil, 0)
+	w.menu.SetLayout(widgets.NewQHBoxLayout())
+	w.menu.Layout().SetContentsMargins(0, 0, 0, 0)
+	w.base.Layout().AddWidget(w.menu)
 
 	openIcon := app.Style().StandardIcon(widgets.QStyle__SP_DialogOpenButton, nil, nil)
-	openButton := widgets.NewQPushButton3(openIcon, "", nil)
-	menu.Layout().AddWidget(openButton)
+	w.openButton = widgets.NewQPushButton3(openIcon, "", nil)
+	w.menu.Layout().AddWidget(w.openButton)
 
-	pathInput := widgets.NewQLineEdit(nil)
-	pathInput.SetPlaceholderText("bms file/folder path")
-	menu.Layout().AddWidget(pathInput)
+	w.pathInput = widgets.NewQLineEdit(nil)
+	w.pathInput.SetPlaceholderText("bms file/folder path")
+	w.menu.Layout().AddWidget(w.pathInput)
 
-	diffCheck := widgets.NewQCheckBox2("diff", nil)
-	menu.Layout().AddWidget(diffCheck)
+	w.diffCheck = widgets.NewQCheckBox2("diff", nil)
+	w.menu.Layout().AddWidget(w.diffCheck)
 
 	checkIcon := app.Style().StandardIcon(widgets.QStyle__SP_DialogApplyButton, nil, nil)
-	checkButton := widgets.NewQPushButton3(checkIcon, "Check", nil)
-	menu.Layout().AddWidget(checkButton)
+	w.checkButton = widgets.NewQPushButton3(checkIcon, "Check", nil)
+	w.menu.Layout().AddWidget(w.checkButton)
 
-	setLogText := false
-	execCheck := func() {
-		progressSnake.Show()
-		base.SetEnabled(false)
-		go func() {
-			log, err := checkBmsLog(pathInput.Text(), language, diffCheck.IsChecked())
-			if err != nil {
-				logText.SetText(err.Error())
-			} else {
-				logText.SetText(log)
-				setLogText = true
-			}
-			progressSnake.Hide()
-			base.SetEnabled(true)
-		}()
-	}
+	//fileDialog := widgets.NewQFileDialog2(nil, "Open bms file/folder", pathInput.Text(), "bms files (*.bms *.bme *.bml *.pms)")
+	w.fileDialog = widgets.NewQFileDialog2(nil, "Open bms folder", "", "bms folder")
+	w.fileDialog.SetFileMode(widgets.QFileDialog__Directory)
+	//fileDialog.SetOption(widgets.QFileDialog__DontUseNativeDialog, true)
 
-	execDiffBmsDir := func(path1, path2 string) {
-		progressSnake.Show()
-		base.SetEnabled(false)
-		go func() {
-			log, err := diffBmsDirLog(path1, path2)
-			if err != nil {
-				logText.SetText(err.Error())
-			} else {
-				logText.SetText(log)
-				setLogText = true
-			}
-			progressSnake.Hide()
-			base.SetEnabled(true)
-		}()
-	}
+	w.openButton.ConnectClicked(func(bool) {
+		w.fileDialog.SetDirectory(w.pathInput.Text())
+		if w.fileDialog.Exec() == int(widgets.QDialog__Accepted) {
+			w.pathInput.SetText(w.fileDialog.SelectedFiles()[0])
+			w.execCheck()
+		}
+	})
 
-	logText.ConnectTextChanged(func() {
-		if setLogText {
-			doc, err := goquery.NewDocumentFromReader(strings.NewReader(logText.ToHtml()))
+	w.checkButton.ConnectClicked(func(bool) {
+		w.execCheck()
+	})
+}
+
+func (w *BmsCheckerWindow) logTextArea() {
+	w.logText = widgets.NewQTextEdit(nil)
+	w.logText.SetFontPointSize(11.0)
+	w.logText.SetReadOnly(true)
+	w.logText.SetLineWrapMode(widgets.QTextEdit__NoWrap)
+	w.logText.SetHorizontalScrollBar(widgets.NewQScrollBar2(core.Qt__Horizontal, nil))
+	w.logText.SetPlaceholderText("Drag and drop bms file/folder!")
+	w.base.Layout().AddWidget(w.logText)
+
+	w.setLogText = false
+
+	w.logText.ConnectTextChanged(func() {
+		if w.setLogText {
+			doc, err := goquery.NewDocumentFromReader(strings.NewReader(w.logText.ToHtml()))
 			if err != nil {
-				logText.SetText(err.Error())
+				w.logText.SetText(err.Error())
 			}
 
 			type levelColor struct {
@@ -145,7 +186,7 @@ func main() {
 			doc.Find("p span").Each(func(i int, s *goquery.Selection) {
 				for _, lc := range levelColors {
 					level := lc.Level_en
-					if language == "ja" {
+					if w.language == "ja" {
 						level = lc.Level_ja
 					}
 					if strings.HasPrefix(s.Text(), level+": ") {
@@ -154,71 +195,70 @@ func main() {
 					}
 				}
 			})
-			setLogText = false
+			w.setLogText = false
 
 			_html, _ := doc.Html()
-			logText.SetHtml(_html)
+			w.logText.SetHtml(_html)
 		}
 	})
+}
 
-	window.ConnectDragEnterEvent(func(e *gui.QDragEnterEvent) {
-		if e.MimeData().HasUrls() {
-			e.AcceptProposedAction()
+func (w *BmsCheckerWindow) execCheck() {
+	w.progressSnake.Show()
+	w.base.SetEnabled(false)
+	go func() {
+		log, err := checkBmsLog(w.pathInput.Text(), w.language, w.diffCheck.IsChecked())
+		if err != nil {
+			w.logText.SetText(err.Error())
+		} else {
+			w.logText.SetText(log)
+			w.setLogText = true
 		}
-	})
-	window.ConnectDropEvent(func(e *gui.QDropEvent) {
-		if e.MimeData().HasUrls() {
-			switch len(e.MimeData().Urls()) {
-			case 1:
-				pathInput.SetText(e.MimeData().Urls()[0].ToLocalFile())
-				execCheck()
-			case 2:
-				path1, path2 := e.MimeData().Urls()[0].ToLocalFile(), e.MimeData().Urls()[1].ToLocalFile()
-				execDiffBmsDir(path1, path2)
-			}
-		}
-	})
+		w.progressSnake.Hide()
+		w.base.SetEnabled(true)
+	}()
+}
 
-	//fileDialog := widgets.NewQFileDialog2(nil, "Open bms file/folder", pathInput.Text(), "bms files (*.bms *.bme *.bml *.pms)")
-	fileDialog := widgets.NewQFileDialog2(nil, "Open bms folder", "", "bms folder")
-	fileDialog.SetFileMode(widgets.QFileDialog__Directory)
-	//fileDialog.SetOption(widgets.QFileDialog__DontUseNativeDialog, true)
-	openButton.ConnectClicked(func(bool) {
-		fileDialog.SetDirectory(pathInput.Text())
-		if fileDialog.Exec() == int(widgets.QDialog__Accepted) {
-			pathInput.SetText(fileDialog.SelectedFiles()[0])
-			execCheck()
+func (w *BmsCheckerWindow) execDiffBmsDir(path1, path2 string) {
+	w.progressSnake.Show()
+	w.base.SetEnabled(false)
+	go func() {
+		log, err := diffBmsDirLog(path1, path2)
+		if err != nil {
+			w.logText.SetText(err.Error())
+		} else {
+			w.logText.SetText(log)
+			w.setLogText = true
 		}
-	})
+		w.progressSnake.Hide()
+		w.base.SetEnabled(true)
+	}()
+}
 
-	checkButton.ConnectClicked(func(bool) {
-		execCheck()
-	})
+func (w *BmsCheckerWindow) setLanguage(lang string) {
+	w.language = lang
+	switch w.language {
+	case "en":
+		w.setting.SetTitle("Setting")
+		w.languageMenu.SetTitle("Language")
+		w.logText.SetPlaceholderText("Drag and drop bms file/folder!")
+		w.pathInput.SetPlaceholderText("bms file/folder path")
+		w.diffCheck.SetText("diff")
+		w.checkButton.SetText("Check")
+		w.fileDialog.SetWindowTitle("Open bms folder")
 
-	changeLanguage = func(lang string) {
-		switch lang {
-		case "en":
-			setting.SetTitle("Setting")
-			languageMenu.SetTitle("Language")
-			logText.SetPlaceholderText("Drag and drop bms file/folder!")
-			pathInput.SetPlaceholderText("bms file/folder path")
-			diffCheck.SetText("diff")
-			checkButton.SetText("Check")
-			fileDialog.SetWindowTitle("Open bms folder")
-		case "ja":
-			setting.SetTitle("設定")
-			languageMenu.SetTitle("言語")
-			logText.SetPlaceholderText("BMSファイル/フォルダをドラッグ&ドロップ!")
-			pathInput.SetPlaceholderText("BMSファイル/フォルダ パス")
-			diffCheck.SetText("差分")
-			checkButton.SetText("チェック")
-			fileDialog.SetWindowTitle("BMSフォルダを開く")
-		}
+		w.actionEn.SetChecked(true)
+	case "ja":
+		w.setting.SetTitle("設定")
+		w.languageMenu.SetTitle("言語")
+		w.logText.SetPlaceholderText("BMSファイル/フォルダをドラッグ&ドロップ!")
+		w.pathInput.SetPlaceholderText("BMSファイル/フォルダ パス")
+		w.diffCheck.SetText("差分")
+		w.checkButton.SetText("チェック")
+		w.fileDialog.SetWindowTitle("BMSフォルダを開く")
+
+		w.actionJa.SetChecked(true)
 	}
-
-	window.Show()
-
-	widgets.QApplication_Exec()
 }
 
 func checkBmsLog(path, lang string, diff bool) (log string, _ error) {
